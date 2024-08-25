580bbaf9649cedaf0c70157a21c914fb4055adca
xzxiong <xzxiong@yeah.net>
2024-08-25 22:31:22 +0800

    chore: calling QueryService::CmdMethod_GOMAXPROCS

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/matrixorigin/matrixone/pkg/common/runtime"
 	"github.com/matrixorigin/matrixone/pkg/defines"
+	"github.com/matrixorigin/matrixone/pkg/logutil"
 	"github.com/matrixorigin/matrixone/pkg/pb/query"
 	qclient "github.com/matrixorigin/matrixone/pkg/queryservice/client"
 	rawzap "go.uber.org/zap"
@@ -51,30 +52,78 @@ func main() {
 		os.Exit(1)
 	}
 
-	uid := uuid.Must(uuid.NewRandom())
+	serviceId := uuid.Must(uuid.NewRandom())
+	// init for rpc env
+	setupMORuntime(serviceId.String())
+
 	rpcCfg := config.GetRpcConfig()
 	logger.Info("init QueryService client")
-	client, err := qclient.NewQueryClient(uid.String(), *rpcCfg)
+	client, err := qclient.NewQueryClient(serviceId.String(), *rpcCfg)
 	if err != nil {
 		logger.Error(err, "failed to init QueryService client")
 		os.Exit(1)
 	}
-
-	// init for rpc env
-	rt := runtime.DefaultRuntimeWithLevel(rawzap.InfoLevel)
-	rt.SetGlobalVariables(runtime.MOProtocolVersion, defines.MORPCLatestVersion)
-	runtime.SetupProcessLevelRuntime(rt)
+	defer client.Close()
 
 	addr := cfg.App.GetRpcAddr("")
 	req := client.NewRequest(query.CmdMethod_GetProtocolVersion)
 	req.GetProtocolVersion = &query.GetProtocolVersionRequest{}
 	deadlineCtx, dcCancel := context.WithTimeout(ctx, cfg.App.RpcTimeout)
-	defer dcCancel()
 	resp, err := client.SendMessage(deadlineCtx, addr, req)
 	if err != nil {
 		logger.Error(err, "failed to request QueryService")
+		dcCancel()
 		os.Exit(1)
 	}
 	logger.Info("GetProtocolVersion", "addr", addr, "version", resp.GetProtocolVersion.Version)
 	client.Release(resp)
+	dcCancel()
+
+	// query GOMAXPROCS
+	req = client.NewRequest(query.CmdMethod_GOMAXPROCS)
+	req.GoMaxProcsRequest = &query.GoMaxProcsRequest{MaxProcs: 0}
+	deadlineCtx, dcCancel = context.WithTimeout(ctx, cfg.App.RpcTimeout)
+	resp, err = client.SendMessage(deadlineCtx, addr, req)
+	if err != nil {
+		logger.Error(err, "failed to request QueryService")
+		dcCancel()
+		os.Exit(1)
+	}
+	logger.Info("GOMAXPROCS query", "addr", addr, "MaxProcs", resp.GoMaxProcsResponse.MaxProcs)
+	client.Release(resp)
+	dcCancel()
+
+	// query GOMAXPROCS
+	req = client.NewRequest(query.CmdMethod_GOMAXPROCS)
+	req.GoMaxProcsRequest = &query.GoMaxProcsRequest{MaxProcs: 6}
+	deadlineCtx, dcCancel = context.WithTimeout(ctx, cfg.App.RpcTimeout)
+	resp, err = client.SendMessage(deadlineCtx, addr, req)
+	if err != nil {
+		logger.Error(err, "failed to request QueryService")
+		dcCancel()
+		os.Exit(1)
+	}
+	logger.Info("GOMAXPROCS query", "addr", addr, "MaxProcs", resp.GoMaxProcsResponse.MaxProcs)
+	client.Release(resp)
+	dcCancel()
+}
+
+func setupMORuntime(serviceId string) {
+	logutil.SetupMOLogger(&logutil.LogConfig{
+		Level:           "info",
+		Format:          "console",
+		Filename:        "",
+		MaxSize:         0,
+		MaxDays:         0,
+		MaxBackups:      0,
+		DisableStore:    true,
+		DisableLog:      false,
+		StacktraceLevel: "panic",
+	})
+	rt := runtime.DefaultRuntimeWithLevel(rawzap.InfoLevel)
+	rt.SetGlobalVariables(runtime.MOProtocolVersion, defines.MORPCLatestVersion)
+	// ## for mo 1.2.*
+	// runtime.SetupProcessLevelRuntime(rt)
+	// ##for mo 1.3.*
+	runtime.SetupServiceBasedRuntime(serviceId, rt)
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/go-logr/logr v1.4.2
 	github.com/go-logr/zapr v1.3.0
 	github.com/google/uuid v1.6.0
-	github.com/matrixorigin/matrixone v1.2.2
+	github.com/matrixorigin/matrixone v0.7.1-0.20240824163825-5b70d659a3b2
 	github.com/spf13/viper v1.3.2
 	go.uber.org/zap v1.26.0
 	sigs.k8s.io/controller-runtime v0.19.0
@@ -25,6 +25,7 @@ require (
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/pprof v0.0.0-20240525223248-4bfdf5a9a2af // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
@@ -36,6 +37,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/panjf2000/ants/v2 v2.7.4 // indirect
 	github.com/pelletier/go-toml v1.2.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -51,6 +53,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
@@ -65,5 +68,16 @@ require (
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 )
 
-//replace github.com/matrixorigin/matrixone v1.3.0 => github.com/xzxiong/matrixone v0.7.1-0.20240824163825-5b70d659a3b2
 replace github.com/xzxiong/mo-query-service-demo => ./
+
+//replace github.com/matrixorigin/matrixone v1.3.0 => github.com/xzxiong/matrixone v0.7.1-0.20240824163825-5b70d659a3b2
+replace github.com/matrixorigin/matrixone v0.7.1-0.20240824163825-5b70d659a3b2 => ../../matrixorigin/matrixone
+
+// copy from github.com/matrixorigin/matrixone
+replace (
+	//github.com/elastic/gosigar v0.14.2 => github.com/matrixorigin/gosigar v0.14.3-0.20240721074812-0d5bde0e9f91
+	github.com/fagongzi/goetty/v2 v2.0.3-0.20230628075727-26c9a2fd5fb8 => github.com/matrixorigin/goetty/v2 v2.0.0-20240611082008-a4de209fff3d
+	github.com/lni/dragonboat/v4 v4.0.0-20220815145555-6f622e8bcbef => github.com/matrixorigin/dragonboat/v4 v4.0.0-20240312080931-1b40809d7cea
+	github.com/lni/goutils v1.3.1-0.20220604063047-388d67b4dbc4 => github.com/matrixorigin/goutils v1.3.1-0.20220604063047-388d67b4dbc4
+//github.com/lni/vfs v0.2.1-0.20220616104132-8852fd867376 => github.com/matrixorigin/vfs v0.2.1-0.20220616104132-8852fd867376
+)
